65e24bbd95c1d5acf910b8bb0ca19fdbdec72755
Struk <Alex.Struk@gov.bc.ca>
2023-04-21 16:23:12 -0700

    fixes

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://digital.gov.bc.ca',
-    supportFile:false,
+    // supportFile:false,
     setupNodeEvents(on, config) {
       require("@datashard/snapshot").tasks(on, config)
     }
--- testing/cypress/e2e/spec.cy.js
+++ testing/cypress/e2e/spec.cy.js
@@ -1,13 +1,19 @@
-require("@datashard/snapshot").register();
+
 
 describe('spec.cy.js', () =>{
     it('should visit',()=>{
         cy.visit('/')
     })
 
-    it('home page snapshot', () =>{
-       cy.visit('/')
-        //    cy.get({'sup':1}).snapshot({name:"homepage"})
-        cy.get('html').snapshot({"snapshotName":"foo","snapshotPath": "cypress/snapshots",json: false});
-    })
+    // it('home page snapshot', () =>{
+    //    cy.visit('https://google.com')
+    // //    cy.get('html').snapshot({"snapshotName":"foo","snapshotPath": "cypress/snapshots",json: true});
+    //     // cy.get('html').snapshot();
+    //     let html = cy.get('html');
+    //     cy.wrap({ bar: html }).snapshot("bar",{
+    //         snapshotName: 'Snapshot Name',          // Overwrite the generated Snapshot name
+    //         snapshotPath: 'cypress/snapshots', // Overwrite where the Snapshot should be stored
+    //         json: true                           // convert DOM elements into JSON
+    //       });
+    // })
 })
