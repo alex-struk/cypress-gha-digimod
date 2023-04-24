8676b8474546db448a8b0901d703b1613fdf9db1
Struk <Alex.Struk@gov.bc.ca>
2023-04-24 09:59:12 -0700

    snapshot test

--- testing/cypress/e2e/spec.cy.js
+++ testing/cypress/e2e/spec.cy.js
@@ -5,15 +5,14 @@ describe('spec.cy.js', () =>{
         cy.visit('/')
     })
 
-    // it('home page snapshot', () =>{
-    //    cy.visit('https://google.com')
-    // //    cy.get('html').snapshot({"snapshotName":"foo","snapshotPath": "cypress/snapshots",json: true});
-    //     // cy.get('html').snapshot();
-    //     let html = cy.get('html');
-    //     cy.wrap({ bar: html }).snapshot("bar",{
-    //         snapshotName: 'Snapshot Name',          // Overwrite the generated Snapshot name
-    //         snapshotPath: 'cypress/snapshots', // Overwrite where the Snapshot should be stored
-    //         json: true                           // convert DOM elements into JSON
-    //       });
-    // })
+    it('home page snapshot', () =>{
+       cy.visit('https://google.com')
+       cy.get('html').snapshot({"snapshotName":"foo","snapshotPath": "cypress/snapshots",json: false});
+        // let html = cy.get('html');
+        // html.snapshot("bar",{
+        //     snapshotName: 'Snapshot Name',          // Overwrite the generated Snapshot name
+        //     snapshotPath: 'cypress/snapshots', // Overwrite where the Snapshot should be stored
+        //     json: true                           // convert DOM elements into JSON
+        //   });
+    })
 })
