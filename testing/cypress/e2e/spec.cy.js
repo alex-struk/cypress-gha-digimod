3354134e8b325b8efba000ceb7209080ab5d8f04
Struk <Alex.Struk@gov.bc.ca>
2023-04-25 15:50:00 -0700

    Squashed commit of the following:
    commit 087c8116b56c31fd043a59b848f460578e04f9d2
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Tue Apr 25 14:07:37 2023 -0700
    
        snapshot test working
    
    commit 74b5b262fe9702a8bffac4f958dcce7559aaff1c
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Tue Apr 25 13:29:45 2023 -0700
    
        fixes
    
    commit 8676b8474546db448a8b0901d703b1613fdf9db1
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Mon Apr 24 09:59:12 2023 -0700
    
        snapshot test
    
    commit 65e24bbd95c1d5acf910b8bb0ca19fdbdec72755
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 16:23:12 2023 -0700
    
        fixes
    
    commit 173e23ec1855521e2cdad1e2a98102587376c441
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 14:41:52 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit 1fa1f4d7da74210aa4bb116995807c0dfc260301
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 14:40:26 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit c50002cf5ffe3168df49e48ff7ee2f5710e95d93
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 14:34:45 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit 165fe2238c2a263edcd6aaee2489359e7d49d1e4
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 14:32:46 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit debf69d133e6435404b73860da74fed3d746397a
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 14:23:36 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit 558039628dae8a5f41d7a91ba3762207f8ce21b3
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 14:20:11 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit 29ba24d7776d0a7d770ca432bc0ef8849c8c7547
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 13:59:31 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit 9f6cb900b9c5f18549195c5b9d50273759ab87f4
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 13:56:25 2023 -0700
    
        fixes
    
    commit c0db3cba889b61a83b792c6b75b07901d63d8aa9
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 11:18:16 2023 -0700
    
        Update index.js
    
    commit 80fab73000a6d438d5839423b80ea51047e695df
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 11:12:08 2023 -0700
    
        Update cypress-simple-manual.yaml
    
    commit 3b765d1e7219c8de4c74ef165575a3d9158cf2a2
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 11:11:54 2023 -0700
    
        email plugin
    
    commit e872689d442c425f6c25c6f603e0c2618ca4d03c
    Merge: 62edda1 d86af42
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 11:02:07 2023 -0700
    
        Merge branch 'test'
    
    commit 62edda1cd9a86ab62809161193e949b5e007c2e9
    Author: Struk <Alex.Struk@gov.bc.ca>
    Date:   Fri Apr 21 10:13:13 2023 -0700
    
        Delete cypress-simple-manual.yaml

--- testing/cypress/e2e/spec.cy.js
+++ testing/cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
+
+
 describe('spec.cy.js', () =>{
     it('should visit',()=>{
         cy.visit('/')
+    })
+
+    it('home page snapshot', () =>{
+        cy.request('/').its('body', {log: false}).then(text => {
+            cy.wrap({ html: text }).snapshot("test",{
+                snapshotName: 'Home',
+                snapshotPath: 'cypress/snapshots',
+                json: true                           
+              });
+          });
     })
 })
