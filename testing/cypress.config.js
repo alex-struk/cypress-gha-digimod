e940c1d431c189335628db709ba44cba6dc79b4e
Struk <Alex.Struk@gov.bc.ca>
2023-04-28 14:53:48 -0700

    fixed viewport

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 const { initPlugin } = require("@frsource/cypress-plugin-visual-regression-diff/plugins")
 
 module.exports = defineConfig({
+  viewportHeight:720,
+  viewportWidth:1280,
   e2e: {
     baseUrl: 'https://digital.gov.bc.ca',
     // supportFile:false,
